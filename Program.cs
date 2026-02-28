51d1885e794725dcba8d506a1c5af8ef5eac5a59
ashaar84@gmail.com <ashaar84@gmail.com>
2026-03-01 00:23:53 +0300

    add new service

--- Program.cs
+++ Program.cs
@@ -33,6 +33,19 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/getweather/{param}", (string param) =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
